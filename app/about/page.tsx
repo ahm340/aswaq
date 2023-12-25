2da61db2f7893013b7309a95022af8a652b70733
hassan mosfer <ahm340@gmail.com>
2023-12-25 07:42:17 +0300

    first commit2

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+
+import Link from "next/link"
+const page = () => {
+  return (
+    <>
+    <div className="m-20">Hello to about page</div>
+    <Link className="bg-lime-100 m-4 p-2 " href="/">Retun to Home page</Link>
+    </>
+  )
+}
+
+export default page
